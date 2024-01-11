4d730db30a3ee9ce5549219da12a65c4c40cb5a3
titantooth <ali.rabieeie@gmail.com>
2024-01-12 01:03:59 +0330

    first up

---
+++ travelaar/app/page.tsx
@@ -0,0 +1,23 @@
+import Hero from '@/components/Hero'
+import Camp from '@/components/camp'
+import Features from '@/components/features'
+import Getapp from '@/components/getapp'
+import Guide from '@/components/guide'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Camp />
+      <Guide />
+      <Features />
+      <Getapp />
+
+
+    
+    
+    
+    </>
+  )
+}
---
+++ travelaar/components/Button.tsx
@@ -0,0 +1,24 @@
+type ButtonProps = {
+    type: 'button' | 'submit';
+    title: string;
+    icon?: string;
+    variant: string;
+}
+
+import Image from 'next/image';
+import React from 'react'
+
+
+const Button = ({type,title, icon, variant}: ButtonProps) => {
+  return (
+    <button 
+    className={`flexCenter gap-3 rounded-full border ${variant}`}
+    type={type}
+    >
+        {icon && <Image src={icon} alt={title} width={24} height={24}/>}    
+        <label className='bold-16 whitespace-nowrap'>{title }</label>
+    </button>
+  )
+}
+
+export default Button
