5ad2dd4d62d9365e097c777e55052017d4d56d7f
titantooth <ali.rabieeie@gmail.com>
2024-01-12 01:10:16 +0330

    removed guide borders

--- travelaar/components/Hero.tsx
+++ travelaar/components/Hero.tsx
@@ -4,7 +4,7 @@ import Button from './Button'
 
 const Hero = () => {
   return (
-    <section className='border-2 border-red-500 max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row'>
+    <section className='max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row'>
       <div className='hero-map' />
 
       {/*LEFT */}
@@ -29,7 +29,7 @@ const Hero = () => {
         </div>
         
       </div>
-      <div className='relative flex flex-1 items-start border-2 border-blue-500'>
+      <div className='relative flex flex-1 items-start'>
               <div className='relative z-20 w-[268px] flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8'>
 
               </div>
