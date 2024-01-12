2889c013cd1a938cde7ca161aceb039f5a8523d6
titantooth <ali.rabieeie@gmail.com>
2024-01-12 16:23:30 +0330

    get app section

--- travelaar/components/Features.tsx
+++ travelaar/components/Features.tsx
@@ -6,7 +6,7 @@ import React from 'react'
 const Features = () => {
   return (
     <section className='flex-col flexCenter overflow-hidden bg-feature-bg bg-center bg-no-repeat py-24'>
-      <div className='max-container padding-container relative w-full justify-end'>
+      <div className='max-container padding-container relative w-full justify-end flex'>
         <div className='flex flex-1 lg:min-h-[900px] '>
           <Image src='/phone.png' alt='phone' width={440} height={1000} className='feature-phone' ></Image>
         </div>
@@ -15,7 +15,7 @@ const Features = () => {
             <Image src='/camp.svg' alt='camp' width={50} height={50} className='absolute left-[-5px] top-[-28px] w-10 lg:w-[50px]' ></Image>
             <h2 className='bold-40 lg:bold-64 '>Our features</h2>
           </div>
-          <ul>
+          <ul className='mt-10 grid gap-10 md:grid-cols-2 lg:mt-20 lg:gap-20 '>
             {FEATURES.map((feature) => (
               <FeatureItem
                title={feature.title}
@@ -49,6 +49,12 @@ const FeatureItem = ({title, icon, variant, description}: FeatureItem) => {
         <Image src={icon} alt='map' width={28} height={28} ></Image>
 
       </div>
+      <h2 className='bold-20 lg:bold-32 mt-5 capitalize'>
+        {title}
+      </h2>
+      <p className='regular-16 bg-white/80 mt-5 text-gray-30 lg:mt-30 lg:bg-none '>
+        {description}
+      </p>
     </li>
   )
 }
